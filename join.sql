27192ef6050a13c8e9a26e2c12d821c18fafbd5b
Bart Kang <bart.kang.0719@gmail.com>
2024-07-24 10:45:32 -0700

    left and right join: it can be interchangable

--- join.sql
+++ join.sql
@@ -1,8 +1,15 @@
-SELECT
-  articles.id AS id,
-  articles.title,
-  users.email
-FROM
-  articles
-INNER JOIN users 
-ON articles.user_id = users.id;
+
+-- SELECT
+--   articles.id AS id,
+--   articles.title,
+--   users.email
+-- FROM
+--   articles
+-- INNER JOIN users 
+-- ON articles.user_id = users.id;
+
+
+SELECT * FROM users
+RIGHT JOIN articles
+ON users.id = articles.user_id;
+-- WHERE articles.user_id IS NOT NULL;
